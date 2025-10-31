ecfc0da7d5bd0f1ebb8d6ace2058a769f70525a6
woidon3k <87819375+woidon3k@users.noreply.github.com>
2025-10-31 12:43:07 +0200

    Revert "first_commit"

--- src/ISenetencePart.java
+++
@@ -1,7 +0,0 @@
-/**
- * A marker interface for classes that can be part of a Sentence,
- * such as Word or Punctuation.
- */
-public interface ISenetencePart {
-    // This is a marker interface, no methods required.
-}
